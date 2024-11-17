13fd84abaf2f4db78643508239aa5db257318752
thomas.zhu <zhulq32@163.com>
2024-11-17 19:42:46 +0800

    refine

--- py/common/average.py
+++ py/common/average.py
@@ -6,25 +6,51 @@ from enum import Enum
 import pandas as pd
 import csv
 
-class csv_average:
-    def __init__(self, path, keywords:list):
-        self.__path = path
+class AveragedList:
+    def __init__(self, keywords:list):
         self.__keywords = keywords
-        self.__parse()
+    
+    @staticmethod
+    def delNone(l_in):
+        return l_in
+    
+    def del1stLast(l_data):
+        return l_data[1:-1] # remove Time and last ','
 
-    def __same_key(self, last, data):
-        for k in self.__key_index:
+    @staticmethod
+    def CsvToList(path:str, delFunc=None):
+        try:
+            csvfile = open(path, newline='')
+        except:
+            assert 0, f'fail to open {path}'
+        csv_reader = csv.reader(csvfile)
+        l_data = []
+        for data in csv_reader:
+            l_data.append(delFunc(data))
+        return l_data
+    
+    @staticmethod
+    def ListToCsv(path:str, data:list):
+        with open(path, 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            for row in data:
+                csvwriter.writerow(row)
+    
+    def Dump(self, data:list):
+        print(self.__keywords)
+        for d in data:
+            print(d)
+
+    def __same_key(self, last, data, key_idx_list):
+        for k in key_idx_list:
             if last[k] != data[k]: return False
         return True
-    
-    def __column_filter(self, l_data):
-        return l_data[1:-1] # remove Time and last ','
     
     def __make_average(self, sum_data):
         num_cols = len(sum_data[0])
         num_rows = len(sum_data)
         assert num_cols, 'no data for average'
-        print(f'{num_cols} cols, {num_rows} rows')
+        # print(f'{num_cols} cols, {num_rows} rows')
         if num_cols == 1: return [float(item) for item in sum_data]
         float_matrix = [[float(item) for item in sublist] for sublist in sum_data]
         column_averages = [0] * num_cols
@@ -32,44 +58,44 @@ class csv_average:
             sum = 0
             for row in range(num_rows):
                 sum += float_matrix[row][col]
-            column_averages[col] = sum
+            column_averages[col] = sum / num_rows
         return column_averages
     
-    def __parse(self):
-        try:
-            csvfile = open(path, newline='')
-        except:
-            assert 0, f'fail to open {path}'
-        csv_reader = csv.reader(csvfile)
-        self.__title = self.__column_filter(next(csv_reader))
-        self.__key_index = []
+    def MakeAverage(self, l_data:list):
+        title = l_data[0]
+        key_idx_list = []
         for k in self.__keywords:
-            self.__key_index.append(self.__title.index(k))
-        self.__averaged_data = []
-        last_data = None
-        for data in csv_reader:
-            f_data = self.__column_filter(data)
-            if last_data is None:
-                last_data = f_data
-                sum_data = [f_data]
+            key_idx_list.append(title.index(k))
+        averaged_data = [title]
+        last_data = l_data[1]
+        sum_data = [last_data]
+        for data in l_data[2:]:
+            if self.__same_key(last_data, data, key_idx_list):
+                sum_data.append(data)
             else:
-                if self.__same_key(last_data, f_data):
-                    sum_data.append(f_data)
-                else:
-                    self.__averaged_data.append(self.__make_average(sum_data))
-                    last_data = f_data
-                    sum_data = [f_data]
-            self.__averaged_data.append(self.__make_average(sum_data))
-    def dump(self):
-        print(self.__title)
-        # print(self.__key_index)
-        print(self.__averaged_data)
-
-two_dimensional_list = []
-if __name__ == "__main__":
-    if sys.argv[1][1] == ':':
-        path = sys.argv[1]
+                averaged_data.append(self.__make_average(sum_data))
+                last_data = data
+                sum_data = [last_data]
+        averaged_data.append(self.__make_average(sum_data))
+        return averaged_data
+    
+def GetRealPath(argv):
+    if argv[1] == ':':
+        return sys.argv
     else:
-        path = os.path.realpath(os.getcwd()) + '\\' + sys.argv[1]
-    pl = csv_average(path, ['Temperature', 'Voltage'])
-    pl.dump()
+        return os.path.realpath(os.getcwd()) + '\\' + argv
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("arg1: csv in path")
+        print("arg2: csv out path")
+    l_data = AveragedList.CsvToList(GetRealPath(sys.argv[1]), delFunc=AveragedList.del1stLast)
+    al = AveragedList(['Temperature', 'Voltage'])
+    data = al.MakeAverage(l_data)
+    if len(sys.argv) > 2:
+        path = GetRealPath(sys.argv[2])
+        AveragedList.ListToCsv(path, data)
+        print(f'save to {path}')
+    else:
+        al.Dump(data)
